07e43c1373c946fd5eca0fc58efa1c8173d3f8db
vanhadson <vanhadson@gmail.com>
2023-01-14 13:55:22 +0300

    modified file src/games/brain-even.js (added two constants minRandomNumber and maxRandomNumber to get rid of magic numbers)

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,11 +2,13 @@ import getRandomNumber from '../helpers/randomNumber.js';
 import gameRounds from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const minRandomNumber = 1;
+const maxRandomNumber = 20;
 
 const isEven = (randomNum) => randomNum % 2 === 0;
 
 const getRoundData = () => {
-  const randomNumber = getRandomNumber(1, 20);
+  const randomNumber = getRandomNumber(minRandomNumber, maxRandomNumber);
   const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [randomNumber, correctAnswer];
 };
