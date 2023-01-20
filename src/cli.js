853a6bd849eb26903bac5a5f075e18d105649712
vanhadson <vanhadson@gmail.com>
2023-01-20 13:39:40 +0300

    modified file src/cli.js (fix some bugs for hexlet-tests)

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export const requestName = () => {
-  console.log("Welcome to the Brain Games!");
-  let askedUserName = readlineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const askedUserName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${askedUserName}!`);
-  };
-
+};
