bcf767a7f60c724aef938794fa5db0ab3b4c18be
vanhadson <vanhadson@gmail.com>
2023-01-22 12:34:13 +0300

    modified src/index.js (console.log-s moved to a separate lines)

--- src/index.js
+++ src/index.js
@@ -15,8 +15,10 @@ const runGame = (description, getRoundData) => {
       console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
       console.log(`Let's try again, ${userName}!`);
       return;
-    } console.log('Correct!');
-  } console.log(`Congratulations, ${userName}!`);
+    } 
+    console.log('Correct!');
+  } 
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default runGame;
