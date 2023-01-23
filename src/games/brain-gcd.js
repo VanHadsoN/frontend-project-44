a5ff4e569287209f81c228a805e5406e5bed997e
vanhadson <vanhadson@gmail.com>
2023-01-23 13:12:44 +0300

    modified src/games/brain-gcd.js (moved const randomNumber1 and const randomNumber2 to the function)

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,8 +4,6 @@ import gameRounds from '../index.js';
 const description = 'Find the greatest common divisor of given numbers.';
 const minDividable = 1;
 const maxDividable = 20;
-const randomNumber1 = getRandomNumber(minDividable, maxDividable);
-const randomNumber2 = getRandomNumber(minDividable, maxDividable);
 
 const gcd = (number1, number2) => {
   if (number2 === 0) {
@@ -14,6 +12,8 @@ const gcd = (number1, number2) => {
   return gcd(number2, number1 % number2);
 };
 const getRoundData = () => {
+  const randomNumber1 = getRandomNumber(minDividable, maxDividable);
+  const randomNumber2 = getRandomNumber(minDividable, maxDividable);
   const randomOperation = `${randomNumber1} ${randomNumber2}`;
   return [randomOperation, String(gcd(randomNumber1, randomNumber2))];
 };
